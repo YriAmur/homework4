6ca2775e9377827698d528f7d33e04fbb0007fd3
YriAmur <yr1979a@gmail.com>
2024-07-23 11:49:53 +0800

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input('Где Вы учитесь: ') #университет Урбан
+print('online', my_string)
+print(my_string .__len__())
+print(my_string .upper())
+print(my_string .upper().lower())
+print(my_string .replace(' ', '#'))
+print(my_string[0])
+print(my_string[-1])
